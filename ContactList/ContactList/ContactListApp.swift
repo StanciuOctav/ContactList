92636f18485935fd6b51a3085c6b0505780cb7ab
StanciuOctav <octavsteaua11@gmail.com>
2024-02-10 09:35:31 +0200

    Created default Xcode project

---
+++ ContactList/ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Ioan-Octavian Stanciu on 10.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
