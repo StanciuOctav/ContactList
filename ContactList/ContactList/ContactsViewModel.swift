354d4976a20c7554c80103f4685c47495d284891
StanciuOctav <octavsteaua11@gmail.com>
2024-02-10 11:20:50 +0200

    Built the ContactList UI, downloading the image for the odd id, implemented navigation logic

--- ContactList/ContactList/ContactsViewModel.swift
+++ ContactList/ContactList/ContactsViewModel.swift
@@ -10,10 +10,11 @@ import SwiftUI
 final class ContactsViewModel: ObservableObject {
     
     @Published var contacts: [PersonModel] = []
+    let imageURL: String = "https://fastly.picsum.photos/id/553/200/200.jpg?hmac=HSLKzqqoxnajv4KjLxYSjZokWcuCCiZLGdRPUoryhXk"
+    private var contactsUrlString: String = "https://gorest.co.in/public/v2/users"
     
     func fetchContacts() async {
-        let urlString: String = "https://gorest.co.in/public/v2/users"
-        guard let url = URL(string: urlString) else { return }
+        guard let url = URL(string: contactsUrlString) else { return }
         if let contactsData = try? Data(contentsOf: url) {
             if let jsonContacts = try? JSONDecoder().decode([PersonModel].self, from: contactsData) {
                 DispatchQueue.main.async { [weak self] in
--- ContactList/ContactList/ContentView.swift
+++ ContactList/ContactList/ContentView.swift
@@ -16,22 +16,64 @@ struct ContentView: View {
             List {
                 Section("CONTACTELE MELE") {
                     ForEach(vm.contacts, id: \.self) { contact in
-                        HStack {
-                            //                    if contact.id % 2 == 0 {
-                            Text("\(contact.name)")
-                            //                    } else {
-                            //                        Text("\(contact.email)")
-                            //                    }
+                        NavigationLink(value: contact) {
+                            HStack {
+                                if contact.id % 2 == 0 {
+                                    Text("\(self.returnNameInitialsFrom(name: contact.name))")
+                                        .frame(width: 40, height: 40)
+                                        .foregroundStyle(.white)
+                                        .padding(5)
+                                        .background(.gray.opacity(0.3))
+                                        .clipShape(Circle())
+                                } else {
+                                    AsyncImage(url: URL(string: vm.imageURL)) { image in
+                                        image
+                                            .resizable()
+                                            .frame(width: 50, height: 50)
+                                            .clipShape(Circle())
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                }
+                                Text("\(contact.name)")
+                                    .padding(.leading, 10)
+                            }
+                            .ignoresSafeArea(.container, edges: [.leading])
                         }
                     }
                 }
             }
-            .navigationTitle("Contacte")
-            .navigationBarTitleDisplayMode(.large)
+            .navigationDestination(for: PersonModel.self) { person in
+                VStack {
+                    // View to display more info about the person
+                }
+                .navigationTitle("\(person.name)")
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Text("Contacte")
+                        .bold()
+                        .font(.title)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        // TODO: Maybe add a new person?
+                    } label: {
+                        Image(systemName: "person.fill.badge.plus")
+                            .foregroundStyle(.gray)
+                    }
+                }
+            }
         }
         .task {
             await vm.fetchContacts()
         }
+    }
+    
+    private func returnNameInitialsFrom(name: String) -> String {
+        return name.split(separator: " ")
+            .map { $0.prefix(1) }
+            .joined()
     }
 }
 
